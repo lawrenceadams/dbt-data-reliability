3d9802fcd1dfed6fd6e9eed9d4e75673607f79a5
Lawrence Adams <lawrencetadams@googlemail.com>
2024-12-01 11:39:55 +0000

    fix: allow multiple INs with DuckDB

--- macros/utils/cross_db_utils/multi_value_in.sql
+++ macros/utils/cross_db_utils/multi_value_in.sql
@@ -32,3 +32,20 @@
         from {{ target_table }}
     )
 {%- endmacro -%}
+
+{%- macro duckdb__edr_multi_value_in(source_cols, target_cols, target_table) -%}
+    -- DuckDB doesn't support multi-value IN, so we emulate it with CONCAT
+    concat(
+        {%- for val in source_cols -%}
+            {{ elementary.edr_cast_as_string(val) -}}
+            {%- if not loop.last %}, {% endif %}
+        {%- endfor %}
+    ) in (
+        select concat({%- for val in target_cols -%}
+            {{ elementary.edr_cast_as_string(val) -}}
+            {%- if not loop.last %}, {% endif %}
+        {%- endfor %})
+        from {{ target_table }}
+    )
+{%- endmacro -%}
+
